92416c0b179b96a1f400d1ddba3a8e29d0f064ef
WAILENGL <162903708+WAILENGL@users.noreply.github.com>
2024-06-05 09:46:31 +0800

    Feat: modified api url to fetch orders with any financial status

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,45 +8,49 @@ const adminApiHandler = (req, res) => {
 };
 
 const customersInfo = (req, res) => {
-	fetch('https://merchantbeware.myshopify.com/admin/api/2024-04/customers.json', {
-	  method: 'GET',
-	  headers: {
-		"Content-Type": "application/json",
-		"X-Shopify-Access-Token": process.env.accessToken
-	  }
-	})
-	  .then(response => response.json())
-	  .then(data => {
-		if (data?.customers?.length > 0) {
-		  fetch('https://merchantbeware.myshopify.com/admin/api/2024-04/orders.json', {
+	fetch(
+		'https://merchantbeware.myshopify.com/admin/api/2024-04/customers.json?',
+		{
 			method: 'GET',
 			headers: {
-			  "Content-Type": "application/json",
-			  "X-Shopify-Access-Token": process.env.accessToken
-			}
-		  })
-		  .then(response2 => response2.json())
-		  .then(ordersData => {
-			if(ordersData?.orders?.length > 0){
-			  const gettingdata = data?.customers?.map(customer => {
-				const ordersDataFilter  = ordersData?.orders?.filter(items => items?.contact_email === customer?.email) 
-  
-				
-  
-				return {
-				  customerInfo : customer,
-				  orderInfo: ordersDataFilter
-				}
-			  })
-			  console.log({gettingdata})
-			  res.status(200).json(gettingdata)
-			}
-		  })
+				'Content-Type': 'application/json',
+				'X-Shopify-Access-Token': process.env.accessToken,
+			},
 		}
-	  })
-	  .catch(err => console.log(err.message))
-  };
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			if (data?.customers?.length > 0) {
+				fetch(
+					'https://merchantbeware.myshopify.com/admin/api/2024-04/orders.json?status=any',
+					{
+						method: 'GET',
+						headers: {
+							'Content-Type': 'application/json',
+							'X-Shopify-Access-Token': process.env.accessToken,
+						},
+					}
+				)
+					.then((response2) => response2.json())
+					.then((ordersData) => {
+						if (ordersData?.orders?.length > 0) {
+							const gettingdata = data?.customers?.map((customer) => {
+								const ordersDataFilter = ordersData?.orders?.filter(
+									(items) => items?.contact_email === customer?.email
+								);
 
-module.exports = {adminApiHandler,
-	customersInfo
-}
+								return {
+									customerInfo: customer,
+									orderInfo: ordersDataFilter,
+								};
+							});
+							console.log({ gettingdata });
+							res.status(200).json(gettingdata);
+						}
+					});
+			}
+		})
+		.catch((err) => console.log(err.message));
+};
+
+module.exports = { adminApiHandler, customersInfo };
